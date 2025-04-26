edd118c81438578f828b6440f01159c52de60488
1malla1 <2111016220032@mhs.ulm.ac.id>
2025-04-26 22:34:37 +0800

    update auth of login using http-only cookie(but still need improvement)

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,20 +3,28 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
-     * Get the path the user should be redirected to when not authenticated.
+     * Get the path the user should be redirected to when they are not authenticated.
      */
-    protected function redirectTo($request): ?string
+    protected function redirectTo(Request $request): ?string
     {
-        if ($request->expectsJson()) {
-            return response()->json([
-                'message' => 'Unauthenticated.',
-            ], 401);
+        return $request->expectsJson() ? null : route('login');
+    }
+
+    public function authenticate($request, array $guards)
+    {
+        $token = $request->cookie('access_token');
+
+        if (empty($token)) {
+            $this->unauthenticated($request, $guards);
         }
 
-        return null;
+        $request->headers->set('Authorization', 'Bearer ' . $token);
+
+        parent::authenticate($request, $guards);
     }
 }
